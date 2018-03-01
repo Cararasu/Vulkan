5f5f68d978d8b7b55deef2f3a698b667fb545879
Thomas Wagner <Cararasuu@gmail.com>
2018-03-01 20:35:29 +0100

    added stuff for new render abstraction

--- main.cpp
+++ main.cpp
@@ -22,6 +22,158 @@
 
 #include <fstream>
 
+
+template<typename T>
+struct ProxyObject{
+	T obj;
+	T* proxyObj = nullptr;
+	
+	inline void update(){
+		if(proxyObj) memcpy(&obj, proxyObj, sizeof(T));
+		printf("Proxy_update\n");
+	}
+};
+//Job system: Fork -> Meet -> Join ...
+
+//Stages
+//	GameTick
+//		GameLogic
+//	FrameTick
+//	GraphicsTick
+//	UpdatePhase
+
+//Stages
+//	WorkStage
+//		GraphicsTick(working on obj)
+//		LogicTick(working on ProxyObjects)
+//			FrameTick + opt. GameTick
+//	SubmitStage
+//		UpdateStage(proxyObj -> obj)
+//		SubmitTick(data -> gpu)
+
+//GameLogic -> Camera Query -> Graphics Queue -> other Queries -> update GPU Data
+
+#include <stdint.h>
+
+struct ObjId{
+	int id, index;
+};
+
+template<int ID, typename... OBJDATAs> struct ObjStore;
+
+template<int TYPEID>
+struct ObjStore <TYPEID>{
+	template<typename TOBJDATA>
+	inline ObjId insert(TOBJDATA& data){
+		assert(false);//Type not defined
+		return {-1,-1};
+	}
+	inline bool delete_obj(ObjId objId){
+		return false;
+	}
+	inline void clean() {}
+	inline void update() {}
+};
+template<int TYPEID, typename OBJDATA, typename... OBJDATAs>
+struct ObjStore <TYPEID, OBJDATA, OBJDATAs...>{
+	std::vector<std::pair<OBJDATA, bool>> obj_list;
+	ObjStore<TYPEID + 1, OBJDATAs...> subStore;
+	template<typename TOBJDATA>
+	inline ObjId insert(TOBJDATA& data){
+		return subStore.insert(data);
+	}
+	inline ObjId insert(OBJDATA& data){
+		for(auto it = obj_list.begin(); it != obj_list.end(); it++) {
+			if(!it->second){
+				it->first = data;
+				return {TYPEID, (int)std::distance(obj_list.begin(), it)};
+			}
+		}
+		obj_list.push_back(std::make_pair(data, true));
+		return {TYPEID, (int)obj_list.size() - 1};
+	}
+	inline bool delete_obj(ObjId objId){
+		if(objId.id == TYPEID){
+			obj_list[objId.index].second = false;
+			return true;
+		}
+		return subStore.delete_obj(objId);
+	}
+	inline void clean(){
+		obj_list.clean();
+		subStore.clean();
+	}
+	inline void update(){
+		for(std::pair<OBJDATA, bool>& p : obj_list){
+			if(p.second) p.first.proxy_obj.update();
+		}
+		subStore.update();
+	}
+};
+
+template< typename... OBJDATAs>
+struct ObjectStore {
+	ObjStore<0, OBJDATAs...> inner_store;
+
+	template<typename OBJDATA>
+	inline ObjId insert(OBJDATA& data){
+		return inner_store.insert(data);
+	}
+	inline bool delete_obj(ObjId objId){
+		return inner_store.delete_obj(objId);
+	}
+	inline void clean(){
+		inner_store.clean();
+	}
+	inline void update(){
+		inner_store.update();
+	}
+};
+struct DefDrawData{
+	glm::dvec3 pos, rot, scale;
+};
+template<uint32_t ID, typename OBJDATA>
+struct DrawObject{
+	const static uint32_t id = ID;
+	
+	ProxyObject<OBJDATA> proxy_obj;
+	uint64_t instanceId;
+	int m_bounding_box;//2x glm::dvec3
+	bool enabled;
+	
+	virtual void draw() = 0;
+};
+struct CullingStrategy {
+//CameraFlagList
+//QuadTree(Box(Index of Obj))
+
+	//virtual void update(ObjectStore<DrawObject>* objectStore) = 0;
+	
+	virtual void addToStrat(DefDrawData& obj) = 0;
+	virtual void removeFromStrat(DefDrawData& obj) = 0;
+};
+struct RenderContext {
+	//ObjectStore<DrawObject, uint32_t> objStore;
+	CullingStrategy* strategy;
+	
+	template<typename OBJDATA>
+	uint64_t createObject();
+	template<typename OBJDATA>
+	void removeObject(uint64_t id);
+};
+
+thread_local struct RenderEnvironment{
+	//update one object at a time
+	template<typename DRAWOBJECT>
+	void update(DRAWOBJECT* obj);
+	//do a partial flush, submit in parts, does not need to be invoked and can be invoked by update
+	template<typename DRAWOBJECT>
+	void partialFinish(DRAWOBJECT* obj);
+	//finish the frame
+	void finish();
+} g_render_environment;
+
+
 void loadDataFile (std::string file, OpaqueObjectDispatcher* dispatcher) {
 
 	std::ifstream input (file, std::ios::binary);
@@ -78,8 +230,29 @@ void loadImage (std::string file, ImageWrapper * imageWrapper, uint32_t index, v
 	stbi_image_free (pixels);
 }
 
+struct WWW{
+	ProxyObject<int> proxy_obj;
+};
+
 int main (int argc, char **argv) {
 	
+	ObjectStore<WWW> store;
+	WWW ww1;
+	WWW ww2;
+	WWW ww3;
+	WWW ww4;
+	ObjId id1 = store.insert(ww1);
+	printf("Id: %d Index: %d\n", id1.id, id1.index);
+	ObjId id2 = store.insert(ww2);
+	printf("Id: %d Index: %d\n", id2.id, id2.index);
+	ObjId id3 = store.insert(ww3);
+	printf("Id: %d Index: %d\n", id3.id, id3.index);
+	ObjId id4 = store.insert(ww4);
+	printf("Id: %d Index: %d\n", id4.id, id4.index); 
+	
+	store.delete_obj(id4);
+	
+	store.update();
 	
 	OpaqueObjectDispatcher* dispatcher = new OpaqueObjectDispatcher();
 	
@@ -269,6 +442,7 @@ int main (int argc, char **argv) {
 	vk::Semaphore imageAvailableSemaphore = createSemaphore();
 	vk::Semaphore drawFinishedSemaphore = createSemaphore();
 
+	
 	while (vWindow->isOpen()) {
 		
 		vWindow->setupFrame();
